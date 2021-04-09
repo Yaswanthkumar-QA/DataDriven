73390d9bffac7be4060459ece2a5868723667584
yaswanthkumar <yaswanthkumarmunagapati@gmail.com>
2021-04-09 17:02:22 +0530

    Data Driven from Excel File

---
+++ src/test/java/testSample.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class testSample {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		dataDriven d=new dataDriven();
+		ArrayList data=d.getData("Delete Profile");
+		
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
+
+	}
+
+}
